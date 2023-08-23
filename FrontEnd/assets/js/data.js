6ea353b88de3387e77716de1c9b6491e4967500a
maison <maisonluck@gmail.com>
2023-08-23 09:54:05 -0300

    style: style mural

--- FrontEnd/assets/js/data.js
+++ FrontEnd/assets/js/data.js
@@ -3,14 +3,15 @@ const resultados = document.getElementById("resultado");
 function preencherTabela(data) {
   data.forEach((item) => {
     const tr = document.createElement("tr");
+    tr.classList.add("bg-white");
 
-    tr.innerHTML = `<td>${item.id}</td>
-    <td>${item.nome}</td>
-    <td>${item.descricao}</td>
-    <td>${item.tipo}</td>
-    <td>${item.local}</td>
-    <td>${item.modelo}</td>
-    <td>${item.data}</td>    
+    tr.innerHTML = `<td class="px-6 py-4">${item.id}</td>
+    <th class="px-6 py-4">${item.nome}</th>
+    <td class="px-6 py-4">${item.descricao}</td>
+    <td class="px-6 py-4">${item.tipo}</td>
+    <td class="px-6 py-4">${item.local}</td>
+    <td class="px-6 py-4">${item.modelo}</td>
+    <td class="px-6 py-4">${item.data}</td>    
     `;
     resultados.appendChild(tr);
   });
